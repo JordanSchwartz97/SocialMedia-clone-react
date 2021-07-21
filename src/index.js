3a2742a0aa1954584944702c4f180a790cf6d5fb
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-21 18:59:57 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppHooks from './components/app';
+
+const jsxElement = <h1>Our React App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(<AppHooks/>, document.getElementById('root'));
