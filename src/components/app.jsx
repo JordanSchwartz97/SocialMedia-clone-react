964d950d177967a64d4ce33f77fe019467b9980b
JordanSchwartz97 <schwartzj1297@gmail.com>
2021-07-28 19:54:23 -0500

    Create post now properly pulls the user's name

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
 import NavBar from './navbar/Navbar';
 import ProfilePage from './profile/profile';
 import LoginPage from './loginpage/LoginPage';
@@ -6,8 +6,11 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './home/Home';
 import RegisterPage from './registerpage/RegisterPage';
 import './app.css'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
  const AppHooks = () => {
-    
+
+   
 
      return(
          <div className="body">
@@ -15,7 +18,7 @@ import './app.css'
             <Switch>
                 <Route path = "/" exact component ={LoginPage} />
                 <Route path = "/home" component = {Home}/>
-                <Route path = "/profile" component ={ProfilePage} />
+                <Route path = "/profile" component ={ProfilePage}  />
                 <Route path = "/register" component ={RegisterPage} />
             </Switch>
         
